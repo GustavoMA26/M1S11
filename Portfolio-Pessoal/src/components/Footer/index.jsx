4da05c06349404b3fd6068fb4edba9e6f52c62c5
Gustavo <gmalmeida26@gmail.com>
2023-08-08 14:43:40 -0300

    feat: creating footer component

---
+++ Portfolio-Pessoal/src/components/Footer/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Footer/style.css"
+
+function Footer () {
+    return (
+        <footer>
+            <div className="footer">
+                <p>Developed by GustavoMA26 for educational purpose | All rights reserved ®</p>
+            </div>
+        </footer>
+    )
+}
+
+export {Footer}
